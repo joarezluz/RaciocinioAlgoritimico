b71eb47d6c2ea0705041cc16ee75ba48570c8b82
Kuantun <joarezluz@gmail.com>
2025-04-24 18:34:01 -0300

    Utilizando Estrutura finalizada

--- jogoBCC_jokenpo (1)/main.py
+++ jogoBCC_jokenpo (1)/main.py
@@ -12,6 +12,8 @@ modo = 0  #padrão = 0
 naveg =[menu,subMenu,modo]# menu e submenu e o modo do jogo
 
 
+
+
 ## Etapa == 1 Jogo iniciado
 ## Etapa == 0 Jogo Finalizado
 
@@ -20,7 +22,7 @@ naveg[0]=1 #iniciar jogo com a intodução
 
 while naveg[0] > 0: # jogo iniciado enquando maior que 0
     if naveg[0] == 1: #introducao
-        print('\n::::::::::::::::\n::::Jokenpo!!:::\n:::::Python:::::\n::::::::::::::::')
+        print('\n..............................\n...........Jokenpo!...........\n............Python............\n::::::::::::::::')
         naveg[0] = 2 # ir para o menu principal
     if naveg[0] == 2: ##menu principal
         menuSel = int(input('::::::::::::::::\n::>[1] Iniciar\n::>[2] Créditos\n::>[]Sair\n>> '))
@@ -33,17 +35,43 @@ while naveg[0] > 0: # jogo iniciado enquando maior que 0
         naveg[1] = 1
         if naveg[1] == 1:
             print('::::::::::::::::\nSELECIONE O MODO\n::::::::::::::::\n::::::::::::::::')
-            naveg[2] = int(input('::=>[1] CPU vs CPU \n::>[0] Voltar\n>> '))
-            if naveg[2] == 0: #sair do modo e voltar ao menu anterior [menu principal]
+            naveg[2] = int(input('::=>[1] CPU vs CPU \n::>[] Voltar\n>> '))
+            if naveg[2] != 1: #sair do modo e voltar ao menu anterior [menu principal]
                 naveg = [2,0,0]
         if naveg[2] == 1: ## CPU X CPU
+            jog1 = "CPU1"
+            jog2 = "CPU2"
             while naveg[2] == 1:
-                sel = ['pedra','papel','tesoura']
+                sel = ['Papel','Pedra','Tesoura']
                 cpu1 = random.choice(sel)
                 cpu2 = random.choice(sel)
-                print(f'::::::::::{modo}::::::\n::::CPU 1::::CPU 2::::::::::::\n::::{cpu1}::X::{cpu2}::::::::::::')
-                menuSel = int(input(f'{cpu1} x {cpu2}::\n::> [1] Continuar\n::> [0] Voltar\n::>'))
-                if menuSel == 0 : naveg = [3,1,0] #reconfigura o menu
+                pontCpu1=0
+                pontCpu2=0
+                ganhador = "Ninguém"
+
+                if ((cpu1 == sel[0] and cpu2 == sel[1])
+                        or (cpu1 == sel[1] and cpu2 == sel[2])
+                        or (cpu1 == sel[2] and cpu2 == sel[0])) :
+                    pontCpu1 += 1
+                    ganhador = jog1 + ' Ganhou'
+
+                if ((cpu2 == sel[0] and cpu1 == sel[1])
+                        or (cpu2 == sel[1] and cpu1 == sel[2])
+                        or (cpu2 == sel[2] and cpu1 == sel[0])) :
+                    pontCpu2 += 1
+                    ganhador = jog2 + 'Ganhou'
+                if cpu1 == cpu2: ganhador = "Empate"
+                print(f'::::::::::::::::\n::::CPU 1::::CPU 2::::::::::::\n::::{cpu1}::X::{cpu2}::::::::::::\n *****{ganhador}!!****')
+                menuSel = int(input(f'{jog1} = {pontCpu1} \n {jog2} = {pontCpu2} \n::> [1] Continuar\n::> [] Voltar\n::>'))
+
+                if pontCpu1 > pontCpu2: ganhador = jog1
+                elif pontCpu2 > pontCpu1:(
+                    ganhador) = jog2
+                else:
+                    ganhador = "Empate"
+                print(f'::::::::::::::::\n::::{ganhador}!!!!\n::::::::::::::::')
+
+                if menuSel != 1 : naveg = [3,1,0] #reconfigura o menu
 
         print('::::::::::::::::\n::::::::::::::::\n::::::::::::::::')
     if naveg[0] == 4: ## Créditos
